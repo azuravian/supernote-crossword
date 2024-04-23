dc4b8face5ed0ac06a31e9f5e45cef0df343539a
Gregory Booth <azuravian@gmail.com>
2024-04-23 13:37:01 -0400

    Update main.js
    Removed NYTC section

--- main.js
+++ main.js
@@ -8,83 +8,6 @@ const dbx = new dropbox.Dropbox({
   accessToken: process.env.DROPBOX_ACCESS_TOKEN,
 });
 
-function getNYTC(date) {
-  return new Promise((resolve, reject) => {
-    const req = https.request({
-      protocol: 'https:',
-      host: 'www.nytimes.com',
-      path: `/svc/crosswords/v2/puzzle/print/${moment(date).format('MMMDDYY')}.pdf`,
-      method: 'GET',
-      headers: {
-        Referer: 'https://www.nytimes.com/crosswords/archive/daily',
-        Cookie: process.env.NYT_COOKIE,
-      },
-    }, (res) => {
-      if (res.statusCode === 200) {
-        const data = [];
-        res.on('error', (err) => {
-          reject(err);
-        });
-        res.on('data', (chunk) => {
-          data.push(chunk);
-        });
-        res.on('end', () => {
-          resolve(Buffer.concat(data));
-        });
-      } else {
-        reject(res.statusCode);
-      }
-    });
-    req.on('error', (err) => {
-      reject(err);
-    });
-    req.end();
-  });
-}
-
-async function nytc(date) {
-  console.log(`Checking ${moment(date).format('YYYY-MM-DD')}'s crossword.`);
-  try {
-    await getNYTC(date);
-    console.log(`Successfully checked ${moment(date).format('YYYY-MM-DD')}'s crossword.`);
-  } catch (error) {
-    console.log(`NYT_COOKIE likely expired. Error: ${error}`);
-    process.exit(1);
-  }
-  date.setDate(date.getDate() + 1);
-  console.log(`Downloading ${moment(date).format('YYYY-MM-DD')}'s crossword.`);
-  data = undefined;
-  try {
-    data = await getNYTC(date);
-    console.log(`Successfully downloaded ${moment(date).format('YYYY-MM-DD')}'s crossword.`);
-  } catch (error) {
-    console.log(`${moment(date).format('YYYY-MM-DD')}'s crossword is not yet released.`);
-    return;
-  }
-  console.log(`Checking if file exists.`);
-  try {
-    await dbx.filesGetMetadata({
-      path: path.join(process.env.DROPBOX_NYTC_PATH, `${moment(date).format('YYYY-MM-DD-ddd')}-crossword.pdf`),
-    });
-    console.log(`File already uploaded.`);
-    return;
-  } catch (error) {
-    console.log(`File not yet uploaded.`);
-  }
-  console.log(`Uploading file.`);
-  try {
-    response = await dbx.filesUpload({
-      path: path.join(process.env.DROPBOX_NYTC_PATH, `${moment(date).format('YYYY-MM-DD-ddd')}-crossword.pdf`),
-      contents: data,
-    });
-    console.log(`Successfully uploaded ${response.result.content_hash}.`);
-    return;
-  } catch (error) {
-    console.log(`DROPBOX_ACCESS_TOKEN likely expired. Error: ${error}`);
-    process.exit(1);
-  }
-}
-
 function getWSJC(date) {
   return new Promise((resolve, reject) => {
     const req = https.request({
@@ -151,8 +74,6 @@ async function wsjc(date) {
 }
 
 async function download(date) {
-  console.log(`NYTC Block`);
-  await nytc(new Date(date.getTime()));
   console.log(`WSJC Block`);
   await wsjc(new Date(date.getTime()));
 }
